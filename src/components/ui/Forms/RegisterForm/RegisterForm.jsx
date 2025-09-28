dea044c789ef52fc3cd637997e3ed2a212364588
Fabien Durousset <fabien.durousset@gmail.com>
2025-09-28 19:20:53 +0200

    form validation amelioration

--- src/components/ui/Forms/RegisterForm/RegisterForm.jsx
+++ src/components/ui/Forms/RegisterForm/RegisterForm.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "@/components/ui/Forms/Form";
 import Button from "@/components/ui/Buttons/Button";
 import Input from "@/components/ui/Inputs/Input";
@@ -11,6 +11,7 @@ export default function RegisterForm() {
     const [repeatPassword, setRepeatPassword] = useState("");
     const [pseudo, setPseudo] = useState("");
     const [message, setMessage] = useState("");
+    const [disabled, setDisabled] = useState(false);
 
     async function handleRegister(e) {
         e.preventDefault();
@@ -33,17 +34,37 @@ export default function RegisterForm() {
         setMessage("Erreur serveur");
         }
     }
-    function validateForm() {
-        
-    }
+    useEffect(() => {
+        function validate() {
+
+			if(!password){
+				setMessage("");
+				setDisabled(true);
+			}
+            if(password !== repeatPassword) {
+                setMessage("Les mots de passe ne correspondent pas");
+               	setDisabled(true);
+            }
+			if(password && password.length < 6) {
+				setMessage("Le mot de passe doit contenir au moins 6 caractères");
+				setDisabled(true);
+			}
+			if(password && password === repeatPassword && password.length >= 6) {
+				setMessage("Ready to go :)");
+				setDisabled(false);
+			}
+          }
+		  validate();
+
+    },[password, repeatPassword])
 
   return (
     <Form onSubmit={handleRegister}>
-      <Input placeholder="Pseudo" value={pseudo} onChange={(e) => setPseudo(e.target.value)} />
-      <Input placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <Input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <Input placeholder="Repeat password" type="password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} />
-      <Button type="submit" disabled= {password != repeatPassword ? true : false}>S’inscrire</Button>
+      <Input name="pseudo"placeholder="Pseudo" value={pseudo} onChange={(e) => setPseudo(e.target.value)} />
+      <Input name="email" placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input name="password" placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Input name="repeat password" placeholder="Repeat password" type="password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} />
+      <Button type="submit" disabled={disabled}>S’inscrire</Button>
       <p>{message}</p>
     </Form>
   );
